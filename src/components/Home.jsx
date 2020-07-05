fe50c3e2516969ae1af34a794849d5cae96f46c8
marcellits <garcia.marcella@hotmail.com>
2020-07-05 17:17:27 -0400

    adding style to Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+function Home() {
+  return (
+    <div>
+        <div className="my_name_box">
+            <div className="w-90pc p-15 flex items-center justify-start"> 
+            <div className="pattern-dots-md my_name_bg px-l5  max-w-20pc overflow-visible ml-5 md-ml-l5">
+            </div>
+                <div className="text_name_box">
+                    <h1 className="my_name_text">MARCELLA GARCIA</h1>
+                    <h1 className="my_name_text_1">WEB DEVELOPER </h1>
+                </div>
+                
+            </div>
+            <div>
+                    
+                </div>    
+        </div>
+    </div>  
+  );
+}
+
+export default Home;
