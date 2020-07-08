aff4a2ceb596bdd48c9c47fd1f835a32488aa720
marcellits <garcia.marcella@hotmail.com>
2020-07-07 20:42:41 -0400

    Adding animation on Home component and style colors.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,63 @@ import Skills from './Skills';
 import Contact from './Contact';
 
 function Home() {
+
+  /*HOME ANIMATION
+  CONTENT EXTRACT FROM */ 
+  
+
+  document.addEventListener('DOMContentLoaded',function(event){
+    // array with texts to type in typewriter
+    var dataText = [ "< html >", "< style >", "< script >"];
+    
+    // type one text in the typwriter
+    // keeps calling itself until the text is finished
+    function typeWriter(text, i, fnCallback) {
+      // chekc if text isn't finished yet
+      if (i < (text.length)) {
+        // add next character to h1
+       document.querySelector("#home_animation").innerHTML = text.substring(0, i+1) +'<span id="span-animation" aria-hidden="true"></span>';
+  
+        // wait for a while and call this function again for next character
+        setTimeout(function() {
+          typeWriter(text, i + 1, fnCallback)
+        }, 100);
+      }
+      // text finished, call callback if there is a callback function
+      else if (typeof fnCallback == 'function') {
+        // call callback after timeout
+        setTimeout(fnCallback, 700);
+      }
+    }
+    // start a typewriter animation for a text in the dataText array
+     function StartTextAnimation(i) {
+       if (typeof dataText[i] == 'undefined'){
+          setTimeout(function() {
+            StartTextAnimation(0);
+          }, 4000);
+       }else if(i < (dataText[i].length)) { // check if dataText[i] exists
+        // text exists! start typewriter animation
+       typeWriter(dataText[i], 0, function(){
+         // after callback (and whole text has been animated), start next text
+         StartTextAnimation(i + 1);
+       });
+      }else{
+        StartTextAnimation(0);
+      }
+    }
+    // start the text animation
+    StartTextAnimation(0);
+  });
+
   return (
     <div>
       <header className="my_name_box App-header w-90pc p-15 flex items-center justify-start">    
+        <h1 id="home_animation">Hello!</h1>   
+        <div>
         <div className="text_name_box">
           <h1 className="my_name_text">MARCELLA GARCIA</h1>
-          <h1 className="web_dev">WEB DEVELOPER </h1>
+          <h1 className="web_dev">FULL STACK WEB DEVELOPER</h1>
+        </div>
         </div>
         <div className="submenu">
           <a href="#projects">PROJECTS</a>
