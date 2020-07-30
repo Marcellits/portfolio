e1ff360851fedc9ed667ebc9f6922d38f40673cc
marcellits <garcia.marcella@hotmail.com>
2020-07-30 16:59:50 -0400

    Css - Changing font, responsiveness adjustments,adding resume

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,85 +1,113 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import axios from "axios";
+import Resume from "../utils/Resume.pdf";
 
 const Contact = () => {
-
-    const [serverState, setServerState] = useState({
-        submitting: false,
-        status: null
+  const [serverState, setServerState] = useState({
+    submitting: false,
+    status: null,
+  });
+  const handleServerResponse = (ok, msg, form) => {
+    setServerState({
+      submitting: false,
+      status: { ok, msg },
+    });
+    if (ok) {
+      form.reset();
+    }
+  };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    setServerState({ submitting: true });
+    axios({
+      method: "post",
+      url: "https://formspree.io/mzbjjabe",
+      data: new FormData(form),
+    })
+      .then((r) => {
+        handleServerResponse(
+          true,
+          "Thank you for your Message! We will respond as soon as possible.",
+          form
+        );
+      })
+      .catch((r) => {
+        handleServerResponse(false, r.response.data.error, form);
       });
-      const handleServerResponse = (ok, msg, form) => {
-        setServerState({
-          submitting: false,
-          status: { ok, msg }
-        });
-        if (ok) {
-          form.reset();
-        }
-      };
-      const handleOnSubmit = e => {
-        e.preventDefault();
-        const form = e.target;
-        setServerState({ submitting: true });
-        axios({
-          method: "post",
-          url:"https://formspree.io/mzbjjabe",
-          data: new FormData(form)
-        })
-          .then(r => {
-            handleServerResponse(true, "Thank you for your Message! We will respond as soon as possible.", form);
-          })
-          .catch(r => {
-            handleServerResponse(false, r.response.data.error, form);
-          });
-      };
-    return (
-        <div className="components" id="contact-me">
-            <h1 className="title-component">CONTACT ME</h1>
-            <div className="contact-me-container">
-            <form  onSubmit={handleOnSubmit}>
-                <div className="form-group">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" id="name" name="name" className="form-control"/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" className="form-control"/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="message">Message</label>
-                    <textarea className="form-control" name="message" id="message" rows="5"></textarea>
-                </div>
-                <button type="submit" className="btn btn-outline-danger" disabled={serverState.submitting}>Send</button>
-                <div>
-                  {serverState.status && (
-                    <p className={!serverState.status.ok ? "errorMsg" : ""}>
-                        <p>
-                        {serverState.status.msg}
-                        </p>
-                    </p>
-                  )}
-                </div>   
-            </form>
-            <div className="follow-me-box">
-                <h4>Follow me!</h4>
-                <div className="row social-medias">
-                    <div class="col">
-                        <a href="https://www.github.com/marcellits">
-                          <i class="fa fa-github fa-4x" aria-hidden="true"/>
-                        </a>
-                        <a href="https://www.linkedin.com/in/marcellits/">
-                          <i class="fa fa-linkedin-square fa-4x" aria-hidden="true" />
-                        </a>  
-                        <a href="https://twitter.com/_marcellits_">
-                          <i class="fa fa-twitter-square fa-4x" aria-hidden="true"/>
-                        </a>
-                      </div>
-                </div>
+  };
+  return (
+    <div className="components" id="contact-me">
+      <h1 className="title-component">CONTACT ME</h1>
+      <div className="contact-me-container">
+        <form onSubmit={handleOnSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" name="name" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message</label>
+            <textarea
+              className="form-control"
+              name="message"
+              id="message"
+              rows="5"
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="btn btn-outline-danger"
+            disabled={serverState.submitting}
+          >
+            Send
+          </button>
+          <div>
+            {serverState.status && (
+              <p className={!serverState.status.ok ? "errorMsg" : ""}>
+                <p>{serverState.status.msg}</p>
+              </p>
+            )}
+          </div>
+        </form>
+        <div className="follow-me-box">
+          <h4>
+            If you want to know more about me and my experience, take a look at
+            my Resume or access my social medias
+          </h4>
+          <div className="row social-medias">
+            <div class="col">
+              <a href={Resume}>
+                Resume
+                <i
+                  id="resume"
+                  class="fa fa-file-text fa-4x"
+                  aria-hidden="true"
+                ></i>
+              </a>
+              <a href="https://www.github.com/marcellits">
+                <i class="fa fa-github-square fa-4x" aria-hidden="true" />
+              </a>
+              <a href="https://www.linkedin.com/in/marcellits/">
+                <i class="fa fa-linkedin-square fa-4x" aria-hidden="true" />
+              </a>
+              <a href="https://twitter.com/_marcellits_">
+                <i class="fa fa-twitter-square fa-4x" aria-hidden="true" />
+              </a>
             </div>
-            </div>   
+          </div>
         </div>
-    )
-}
-
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,79 +1,65 @@
-import React from 'react';
+import React from "react";
 import Easy from "../images/Easyweather.png";
 import Smart from "../images/Smartagenda.png";
 import Trivia from "../images/trivia.gif";
 
 const Projects = () => {
+  const projs = [
+    {
+      name: "Easy Weather",
+      source: Easy,
+      alt: "Easy Weather screenshot",
+      text:
+        "Easy Weather is a React app that pulls weather information and displays tips about the weather condition of the day. Just type the name of the city and voilà! To manipulate date and time we used Moment.js and to stylize we used React Bootstrap, such as Button and Cards to display the forecast. In addition, we used a typewriter effect on the home page, handling CSS properties to display it.",
+      href: "https://easy-weather-wyncode.herokuapp.com",
+    },
+    {
+      name: "SmartAgenda",
+      source: Smart,
+      alt: "SmartAgenda screenshot",
+      text:
+        "SmartAgenda is a web application for business owners to keep track of client information as well as appointments which are displayed on their calendar. Javascript, MongoDB, React, Express and Node.js were used to develop this project and used FullCalendar.io library, Moment.js and React Date-Picker in the frontend. This project is under continuing improvement for next releases.",
+      href: "https://smartagenda-wyncode.herokuapp.com",
+    },
+    {
+      name: "Next Tech Trivia",
+      source: Trivia,
+      alt: "Trivia gif screenshot",
+      text:
+        " Next Tech Trivia is an interactive game for Google Cloud Next '20: OnAir. Our amazing team developed dynamic app so the usercan enjoy an awesome experience! If user answers 4-5 questions correctly, user can spin the wheel for a prize! This project is coded in Javascript + MERN Stack. To build spin wheel, we applied Canvas. We also used 3 API's: Gitfit as rewards, SendGrid to send emails to users, and Cloudinary to store images.",
+      href: "https://github.com/Marcellits/trivia-game",
+    },
+  ];
 
-    return (
-        <div id="projects " className="components" >
-            <h1  id="projects-title" className="title-component">PROJECTS</h1>
-            <div className="cards">
-                <div className="pattern-diagonal-lines-sm gray-lighter">
-                    <div className="card"  >
-                        <img className="card-img-top" src={Easy} alt="Imagem de capa do card"/>
-                        <div className="card-body">
-                            <h5 className="card-title">Easy Weather</h5>
-                            <p className="card-text font-text-card">
-                            Easy Weather is a React app that pulls weather information and 
-                            displays tips about the weather condition of the day. 
-                            Just type the name of the city and voilà!   
-                            To manipulate date and time we used Moment.js and to stylize we used React Bootstrap, 
-                            such as Button and Cards to display the forecast. 
-                            In addition, we used a typewriter effect on the home page, handling CSS properties 
-                            to display it.
-                            </p>
-                            <a href="https://easy-weather-wyncode.herokuapp.com" class="btn btn-outline-light">Click here</a>
-                        </div>
-                    </div>
+  return (
+    <div id="projects " className="components">
+      <h1 id="projects-title" className="title-component">
+        PROJECTS
+      </h1>
+      <div className="cards">
+        {projs.map((proj) => {
+          return (
+            <div className="pattern-diagonal-lines-sm gray-lighter">
+              <div className="card" key={proj.key}>
+                <img
+                  className="card-img-top"
+                  src={proj.source}
+                  alt={proj.alt}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{proj.name}</h5>
+                  <p className="card-text font-text-card">{proj.text}</p>
+                  <a href={proj.href} class="btn btn-outline-light">
+                    Click here
+                  </a>
                 </div>
-                <div className="pattern-diagonal-lines-sm gray-lighter">
-                    <div className="card" >
-                        <img className="card-img-top" src={Smart} alt="Imagem de capa do card"/>
-                        <div className="card-body">
-                            <h5 className="card-title">SmartAgenda</h5>
-                            <p className="card-text font-text-card">
-                                SmartAgenda is a web application for business owners to keep track of client information
-                                as well as appointments which are displayed on their calendar. 
-                                Javascript, MongoDB, React, Express and Node.js were used to develop this project and used 
-                                FullCalendar.io library, Moment.js and React Date-Picker in the frontend. 
-                                This project is under continuing improvement for next releases.
-                                
-                            </p>
-                            <a href="https://smartagenda-wyncode.herokuapp.com" class="btn btn-outline-light">Click here</a>
-                        </div>
-                    </div>
-                </div>    
-                <div className="pattern-diagonal-lines-sm gray-lighter">
-                    <div className="card" >
-                        <img className="card-img-top" src={Trivia} alt="Imagem de capa do card"/>
-                        <div className="card-body">
-                            <h5 className="card-title">Next Tech Trivia</h5>
-                            <p className="card-text font-text-card">
-                                Next Tech Trivia is an interactive game for Google Cloud Next '20: OnAir'. 
-                                Our amazing team developed dynamic app so the user can enjoy an awesome experience! 
-                                If user answers 4-5 questions correctly, user can spin the wheel for a prize!
-                                <br/>
-                                This project is coded in Javascript + MERN Stack. To build spin wheel, 
-                                we applied Canvas. We also used 3 API's:
-                                Gitfit as rewards, SendGrid to send emails to users, and 
-                                Cloudinary to store images. 
-                            </p>
-                            <a href="https://github.com/Marcellits/trivia-game" class="btn btn-outline-light">Click here</a>
-                        </div>
-                    </div>
-                </div>    
+              </div>
             </div>
-        </div>
-    )
-}
-
-
-
-
-
-
-
-
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
